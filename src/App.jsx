74dd5bf53ed67c75b9a6a993b5c613d196d81f0c
XOKEESH <hello@kishamartin.com>
2024-10-30 10:18:58 -0700

    Challenge 1 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
 const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false)
+  const [userInfo, setUserInfo] = useState({
+    firstName: 'Kisha', 
+    lastName: 'Martin',   
+    hasPets: true,      
+    age: 21
+  })
+
   return (
-    <h1>Hello world!</h1>
-  );
-};
+    <div className={isDarkMode ? 'dark' : 'light'}>
+      <h1>Hello world!</h1>
+      <h1>User Information</h1>
+      <p>First Name: {userInfo.firstName}</p>
+      <p>Last Name: {userInfo.lastName}</p>
+      <p>Has Pets: {userInfo.hasPets ? 'Yes' : 'No'}</p>
+      <p>Age: {userInfo.age}</p>
+
+      <p>Hello, my name is {userInfo.firstName} {userInfo.lastName}, I am {userInfo.age} years old, and I {userInfo.hasPets ? 'have pets.' : 'do not have pets.'}</p>
+    </div>
+
+  )
+}
 
 export default App;
